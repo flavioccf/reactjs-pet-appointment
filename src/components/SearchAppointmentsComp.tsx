9f9bee1d6b9bbfd56d619b52b38825964a553bd1
flavioccf <flavioccf@hotmail.com>
2021-03-21 02:26:09 -0300

    chage search components

---
+++ src/components/SearchAppointmentsComp.tsx
@@ -0,0 +1,77 @@
+import { Input, Select } from "antd";
+import { SortAscendingOutlined, SortDescendingOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import { SearchOptions } from "../interfaces/SearchOptions";
+import AppointmentApi from "../services/apt_api";
+import { Appointment } from "../interfaces/Appointment";
+
+const SearchAppointmentsComp = ({
+    setMyAppointments
+  }: {
+    setMyAppointments: Function
+  }) => {
+    const initSearch: SearchOptions = {
+        sort: "aptDate",
+        order: "desc",
+        filter: "",
+      };
+    const [searchParams, setSearchParam] = useState(initSearch);
+
+    useEffect(() => {
+        const api = new AppointmentApi();
+        const loadApt = async () => {
+            console.log(searchParams);
+            const apts = await api.getAppointmentList(searchParams);
+            console.log(apts);
+            setMyAppointments(apts);
+          }
+        loadApt();
+      }, [searchParams, setMyAppointments]);
+
+    return(
+        <>
+        <Input
+        addonAfter={
+          <>
+            <Select placeholder="Sort By" className="select-before"
+              onSelect={(e) => {
+                setSearchParam((prevState) => ({
+                  ...prevState,
+                  sort: e.toString(),
+                }))
+              }}
+            >
+              <Select.Option value="petName">Pet</Select.Option>
+              <Select.Option value="ownerName">Owner</Select.Option>
+              <Select.Option value="aptDate">Date</Select.Option>
+            </Select>
+            <Select defaultValue="desc" className="select-before"
+              onSelect={(e) => {
+                setSearchParam((prevState) => ({
+                  ...prevState,
+                  order: e.toString(),
+                }))
+              }}
+            >
+              <Select.Option value="asc">Asc <SortAscendingOutlined /></Select.Option>
+              <Select.Option value="desc">Desc <SortDescendingOutlined /></Select.Option>
+            </Select>
+          </>
+        }
+        placeholder="Search appointments"
+        onChange={(e) =>
+          setSearchParam((prevState) => ({
+            ...prevState,
+            filter: e.target.value,
+          }))
+        }
+      />
+        </>
+    );
+};
+
+export default SearchAppointmentsComp;
+
+function setSearchParam(arg0: (prevState: any) => any) {
+    throw new Error("Function not implemented.");
+}
--- src/pages/ListAppointments.tsx
+++ src/pages/ListAppointments.tsx
@@ -1,75 +1,23 @@
-import { Input, PageHeader, Select, Spin } from "antd";
-import { useEffect, useRef, useState } from "react";
+import { PageHeader, Spin } from "antd";
+import { createRef, useEffect, useRef, useState } from "react";
 import AppointmentComp from "../components/AppointmentComp";
 import { Appointment } from "../interfaces/Appointment";
 import AppointmentApi from "../services/apt_api";
-import { LoadingOutlined, SortAscendingOutlined, SortDescendingOutlined } from "@ant-design/icons";
-import { SearchOptions } from "../interfaces/SearchOptions";
+import { LoadingOutlined } from "@ant-design/icons";
 import UpdateModalComp from "../components/UpdateModalComp";
+import SearchAppointmentsComp from "../components/SearchAppointmentsComp";
+import { SearchOptions } from "../interfaces/SearchOptions";
 
 function ListAppointments() {
   const childRef = useRef();
-  const [myAppointments, setMyAppointments] = useState([]);
+  const [myAppointments, setMyAppointments] = useState<Array<Appointment>>([]);
   const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
-  const initSearch: SearchOptions = {
-    sort: "aptDate",
-    order: "desc",
-    filter: "",
-  };
-
-  console.log(childRef.current);
-
-  const [searchParams, setSearchParam] = useState(initSearch);
-
-  useEffect(() => {
-    const api = new AppointmentApi();
-    const loadApt = async () => {
-      const apts = await api.getAppointmentList(searchParams);
-      setMyAppointments(apts);
-    };
-    loadApt();
-  }, [searchParams]);
 
   return (
     <>
       <PageHeader className="site-page-header" title="List Appointments" />
 
-      <Input
-        addonAfter={
-          <>
-            <Select placeholder="Sort By" className="select-before"
-              onSelect={(e) => {
-                setSearchParam((prevState) => ({
-                  ...prevState,
-                  sort: e.toString(),
-                }))
-              }}
-            >
-              <Select.Option value="petName">Pet</Select.Option>
-              <Select.Option value="ownerName">Owner</Select.Option>
-              <Select.Option value="aptDate">Date</Select.Option>
-            </Select>
-            <Select defaultValue="desc" className="select-before"
-              onSelect={(e) => {
-                setSearchParam((prevState) => ({
-                  ...prevState,
-                  order: e.toString(),
-                }))
-              }}
-            >
-              <Select.Option value="asc">Asc <SortAscendingOutlined /></Select.Option>
-              <Select.Option value="desc">Desc <SortDescendingOutlined /></Select.Option>
-            </Select>
-          </>
-        }
-        placeholder="Search appointments"
-        onChange={(e) =>
-          setSearchParam((prevState) => ({
-            ...prevState,
-            filter: e.target.value,
-          }))
-        }
-      />
+      <SearchAppointmentsComp setMyAppointments={setMyAppointments}></SearchAppointmentsComp>
 
       {myAppointments.length === 0 && (
         <Spin
